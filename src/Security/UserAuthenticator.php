9c2329864ed286acab99a0c3d58621b544ec29e3
lucasfarroni <lucas.farroni@thedots.lu>
2022-07-18 17:18:24 +0200

    form inscription

--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -46,9 +46,9 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
-        // return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        // For example: redir l'utilisateur à la connexion vers home
+         return new RedirectResponse($this->urlGenerator->generate('home'));
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl(Request $request): string
